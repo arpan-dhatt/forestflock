3532a299b10418e49965ceaac6265ade26e150ca
Arpan Dhatt <ary.dhatt@gmail.com>
2021-06-25 23:10:36 -0500

    Template code

---
+++ ForestFlock/ForestFlock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ForestFlock
+//
+//  Created by Arpan Dhatt on 6/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ForestFlock/ForestFlock/ForestFlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForestFlockApp.swift
+//  ForestFlock
+//
+//  Created by Arpan Dhatt on 6/25/21.
+//
+
+import SwiftUI
+
+@main
+struct ForestFlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
