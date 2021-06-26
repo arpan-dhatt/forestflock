a64fe08cd35e63095e1d3d605278a6bc99b1f1e7
Arpan Dhatt <ary.dhatt@gmail.com>
2021-06-26 01:00:36 -0500

    Added backend deps and started mesh egress code

---
+++ forest-flock-backend/server/src/mesh_egress.rs
@@ -0,0 +1,27 @@
+use futures_channel::mpsc::UnboundedSender;
+use tokio::{io::AsyncReadExt, net::{TcpListener, TcpStream}};
+
+use crate::network_types::MeshUpdate;
+
+
+pub async fn mesh_listener(event_stream: UnboundedSender<MeshUpdate>) {
+    let listener = TcpListener::bind("0.0.0.0:9090").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+
+        let stream = event_stream.clone();
+        tokio::spawn(async move {
+            decode_event(socket, stream).await;
+        });
+    }
+}
+
+async fn decode_event(mut socket: TcpStream, mut event_stream: UnboundedSender<MeshUpdate>) {
+    let buffer_size: usize = socket.read_u32().await.unwrap() as usize;
+    let mut buffer = vec![0u8; buffer_size];
+    socket.read_exact(&mut buffer).await.unwrap();
+    if let Ok(update)  = serde_json::from_slice(&buffer) {
+        event_stream.start_send(update).unwrap();
+    }
+}
