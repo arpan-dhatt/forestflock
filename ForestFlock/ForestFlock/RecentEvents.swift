bda6b3ce06ce5bf91bcf03235bd69b61eb7f11af
SaakethC <saakethrchenna@gmail.com>
2021-06-26 02:52:09 -0500

    added the map view and the modal/sheets

--- ForestFlock/ForestFlock/RecentEvents.swift
+++ ForestFlock/ForestFlock/RecentEvents.swift
@@ -9,15 +9,24 @@ import SwiftUI
 
 struct RecentEvents: View {
     @State var collapsed = true
+    @State var showingSheet = false
     
     var body: some View {
         ZStack{
             HStack{
                 HStack{
                 VStack{
-                    Text("Recent Events")
+                    Text("Recent Events").font(.title)
+                    ScrollView{
+                        RecentEventCard(messege: "An Wild Vivek Detected", type: "Animals", latitude: 100.0, longitude: 100.0, picture: "ladybug", collapsed: $collapsed).padding(.vertical).onTapGesture {
+                            showingSheet.toggle()
+                        }
+                        RecentEventCard(messege: "An Wild Vivek Detected", type: "Animals", latitude: 100.0, longitude: 100.0, picture: "ladybug", collapsed: $collapsed).padding(.vertical)
+                        RecentEventCard(messege: "An Wild Vivek Detected", type: "Animals", latitude: 100.0, longitude: 100.0, picture: "ladybug", collapsed: $collapsed).padding(.vertical)
+                        
+                    }
                     Spacer()
-                }.frame(minWidth: 0, maxWidth: collapsed ? 0 : 250, minHeight: 100).animation(.easeIn).transition(.slide).padding().font(.title).background(Color.white).shadow(radius: 10.0)
+                }.frame(minWidth: 0, maxWidth: collapsed ? 0 : 400, minHeight: 100).animation(.easeIn).transition(.slide).padding().background(Color.white).shadow(radius: 10.0)
                 VStack{
                     Button(action: {collapsed.toggle()}){
                         Image(systemName: self.collapsed ? "chevron.right" : "chevron.left").font(.system(size: 50))
@@ -27,6 +36,8 @@ struct RecentEvents: View {
                 }
                 Spacer()
             }
+        }.sheet(isPresented: $showingSheet){
+            Text("Hello")
         }
     }
 }
